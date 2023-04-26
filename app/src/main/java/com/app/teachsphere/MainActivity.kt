bfc142f8fb02f38b9e5255afa85cf161b7e3ddda
anjeliputeri <anjeliputeri92@gmail.com>
2023-04-27 06:42:23 +0700

    add home

--- app/src/main/java/com/app/teachsphere/MainActivity.kt
+++ app/src/main/java/com/app/teachsphere/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.app.teachsphere
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.button)
+
+        btn.setOnClickListener{
+            val pindah = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(pindah)
+        }
     }
 }
