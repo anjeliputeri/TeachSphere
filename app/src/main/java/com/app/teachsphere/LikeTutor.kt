f83bd881724d3d342d4edb3bcebec87374363a7d
yevray <septirayyevi@gmail.com>
2023-05-01 20:19:04 +0700

    BestTutor dan LikeTutor

---
+++ app/src/main/java/com/app/teachsphere/LikeTutor.kt
@@ -0,0 +1,11 @@
+package com.app.teachsphere
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LikeTutor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_like_tutor)
+    }
+}
