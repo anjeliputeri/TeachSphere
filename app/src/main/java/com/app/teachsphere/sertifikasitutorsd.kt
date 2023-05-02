ada771f99da25e00a68a022655b742ff910cc12a
Lutfia <lutfiabadaruddin@gmail.com>
2023-05-03 05:23:53 +0700

    sertifikat tutor SD Part 1

---
+++ app/src/main/java/com/app/teachsphere/sertifikasitutorsd.kt
@@ -0,0 +1,11 @@
+package com.app.teachsphere
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class sertifikasitutorsd : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sertifikasitutorsd)
+    }
+}
