92ff3d37016b162e808fa270e306f20375c8c56a
akmalsakirin <d1041201050@student.untan.ac.id>
2023-05-03 22:11:02 +0700

    PROFIL SISWA DAN TUTOR

---
+++ app/src/main/java/com/app/teachsphere/TutorSosmedActivity.kt
@@ -0,0 +1,11 @@
+package com.app.teachsphere
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TutorSosmedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutor_sosmed)
+    }
+}
