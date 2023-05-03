f82b78e81b022b6640cb88035eea7944d5a9b85b
anjeliputeri <anjeliputeri92@gmail.com>
2023-05-04 01:09:10 +0700

    add bottom navigation to tutor

---
+++ app/src/main/java/com/app/teachsphere/tutor/nav/ui/siswa/SiswaViewModel.kt
@@ -0,0 +1,13 @@
+package com.app.teachsphere.tutor.nav.ui.siswa
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SiswaViewModel : ViewModel() {
+
+//    private val _text = MutableLiveData<String>().apply {
+//        value = "This is home Fragment"
+//    }
+//    val text: LiveData<String> = _text
+}
