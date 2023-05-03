50cb5553a261a70e8180c8fec806f819b5f2e8a7
akmalsakirin <d1041201050@student.untan.ac.id>
2023-05-03 20:50:13 +0700

    Lengkapi Profil Siswa

---
+++ app/src/main/java/com/app/teachsphere/TutorPendidikanActivity.kt
@@ -0,0 +1,11 @@
+package com.app.teachsphere
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TutorPendidikanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutor_pendidikan)
+    }
+}
