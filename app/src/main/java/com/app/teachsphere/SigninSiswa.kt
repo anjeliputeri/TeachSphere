3693a552d859755079a3a7387a57a9de428d0560
anjeliputeri <anjeliputeri92@gmail.com>
2023-05-03 19:36:56 +0700

    fix

--- app/src/main/java/com/app/teachsphere/SigninSiswa.kt
+++ app/src/main/java/com/app/teachsphere/SigninSiswa.kt
@@ -1,11 +1,20 @@
 package com.app.teachsphere
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SigninSiswa : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin_siswa)
+
+        val register = findViewById<TextView>(R.id.register)
+
+        register.setOnClickListener{
+            val goRegis = Intent(this@SigninSiswa, registrasi::class.java)
+            startActivity(goRegis)
+        }
     }
 }
