1cab4cd29870e1219a1ee47764817336b8887b00
anjeliputeri <anjeliputeri92@gmail.com>
2023-05-04 02:04:42 +0700

    chat

---
+++ app/src/main/java/com/app/teachsphere/siswa/ChatFinishTutor.kt
@@ -0,0 +1,14 @@
+package com.app.teachsphere.siswa
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.app.teachsphere.R
+
+class ChatFinishTutor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tutor_chatfinish)
+    }
+}
--- app/src/main/java/com/app/teachsphere/siswa/ChatTutorActivity.kt
+++ app/src/main/java/com/app/teachsphere/siswa/ChatTutorActivity.kt
@@ -1,12 +1,21 @@
 package com.app.teachsphere.siswa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.app.teachsphere.R
 
 class ChatTutorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tutor_chat)
+
+        val chatFinish = findViewById<TextView>(R.id.textView11)
+
+        chatFinish.setOnClickListener {
+            val goFinish = Intent(this@ChatTutorActivity, ChatFinishTutor::class.java)
+            startActivity(goFinish)
+        }
     }
 }
