20681ce88ebcfcfff031bb8d48e240341ad9f301
anjeliputeri <anjeliputeri92@gmail.com>
2023-05-10 17:16:56 +0700

    fix feedback

--- app/src/main/java/com/app/teachsphere/siswa/TutorProfile.kt
+++ app/src/main/java/com/app/teachsphere/siswa/TutorProfile.kt
@@ -3,6 +3,7 @@ package com.app.teachsphere.siswa
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import androidx.viewpager.widget.ViewPager
 import com.app.teachsphere.R
@@ -14,6 +15,7 @@ class TutorProfile : AppCompatActivity() {
     lateinit var tabLayout: TabLayout
     lateinit var viewPager: ViewPager
     lateinit var iHome: ImageView
+    lateinit var bChat: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +24,16 @@ class TutorProfile : AppCompatActivity() {
         tabLayout = findViewById(R.id.tabLayout)
         viewPager = findViewById(R.id.viewpager)
         iHome = findViewById(R.id.ivHomeSiswa)
+        bChat = findViewById(R.id.btnPilihTutor)
 
         iHome.setOnClickListener{
             val goHome = Intent(this@TutorProfile, HomeActivity::class.java)
             startActivity(goHome)
+        }
+
+        bChat.setOnClickListener{
+            val goChat = Intent(this@TutorProfile, ChatTutorActivity::class.java)
+            startActivity(goChat)
         }
 
         tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_baseline_person_24))
